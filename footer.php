d2e3bf001d0aa908a6c56e05c2547c5a7ce7aba5
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-06-25 18:15:01 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
-				<div id="inner-footer" class="wrap cf">
+				<div id="inner-footer">
 
 					<nav role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+    					'container_class' => 'footer-links',         // class of container (should you choose to use it)
     					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+    					'menu_class' => 'nav footer-nav',            // adding custom nav class
     					'theme_location' => 'footer-links',             // where it's located in the theme
     					'before' => '',                                 // before the menu
     					'after' => '',                                  // after the menu
@@ -26,9 +26,8 @@
 
 		</div>
 
-		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
 
 	</body>
 
-</html> <!-- end of site. what a ride! -->
+</html>
