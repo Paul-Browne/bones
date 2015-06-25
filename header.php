ff5890a38d813a22981f23eb1446a276ecc90c8c
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-06-25 14:17:46 +0300

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	<head>
 	  
-	  <?php // regular meta stuff ?>
+		<?php // regular meta stuff ?>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,29 +13,29 @@
 		
 		<?php // favicons, apple icons, android icons and windows tiles ?>
 		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/android-chrome-192x192.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/library/icons/manifest.json">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#2d89ef">
-    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/icons/mstile-144x144.png">
-    <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/library/icons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
-    
-    <?php // site wide S.E.O meta ?>
-    <meta name="robots" content="index, follow">
-    <meta name="author" content="Your Name">
-    <meta property="og:site_name" content="yoursite.com">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-72x72.png">
+    		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-76x76.png">
+    		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-114x114.png">
+    		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-120x120.png">
+    		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-144x144.png">
+    		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-152x152.png">
+    		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/library/icons/apple-touch-icon-180x180.png">
+    		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon-32x32.png" sizes="32x32">
+    		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/android-chrome-192x192.png" sizes="192x192">
+    		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon-96x96.png" sizes="96x96">
+    		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon-16x16.png" sizes="16x16">
+    		<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/library/icons/manifest.json">
+    		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/icons/favicon.ico">
+    		<meta name="msapplication-TileColor" content="#2d89ef">
+    		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/icons/mstile-144x144.png">
+    		<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/library/icons/browserconfig.xml">
+    		<meta name="theme-color" content="#ffffff">
+    	
+    		<?php // site wide S.E.O meta ?>
+    		<meta name="robots" content="index, follow">
+    		<meta name="author" content="Your Name">
+    		<meta property="og:site_name" content="yoursite.com">
 		
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		<script>!function(){if("-ms-user-select"in document.documentElement.style&&navigator.userAgent.match(/IEMobile\/10\.0/)){var a=document.createElement("style");a.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.getElementsByTagName("head")[0].appendChild(a)}}();</script>
@@ -44,7 +44,7 @@
 	</head>
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-	  <script>!function(){var a=document.getElementsByTagName("body")[0],b=document.createElement("div");a.appendChild(b),b.id="loader",b.className="loading";var c=document.getElementById("loader");window.onload=function(){c.className="loaded",setTimeout(function(){a.removeChild(c)},1e3)},window.onbeforeunload=function(){a.id="byebye"}}();</script>
+		<script>!function(){var a=document.getElementsByTagName("body")[0],b=document.createElement("div");a.appendChild(b),b.id="loader",b.className="loading";var c=document.getElementById("loader");window.onload=function(){c.className="loaded",setTimeout(function(){a.removeChild(c)},1e3)},window.onbeforeunload=function(){a.id="byebye"}}();</script>
 		<div id="container">
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 				<div id="inner-header" class="">
