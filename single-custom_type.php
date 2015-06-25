737be75da129435206d0832f63097d89cbd05bfa
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-06-25 19:36:09 +0300

    Update single-custom_type.php

--- single-custom_type.php
+++ single-custom_type.php
@@ -3,12 +3,12 @@
 <div id="inner-content">
 <main id="main" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article">
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
 <header class="article-header">
 <h1 class="single-title custom-post-type-title"><?php the_title(); ?></h1>
-<p class="byline vcard"><?php
-printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) ), get_the_term_list( $post->ID, 'custom_cat', ' ', ', ', '' ) );
-?></p>
+<p class="byline vcard">
+<?php printf( __( 'Posted <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) ), get_the_term_list( $post->ID, 'custom_cat', ' ', ', ', '' ) ); ?>
+</p>
 </header>
 <section class="entry-content">
 <?php
