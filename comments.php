97383fda0bccee3e9ef879b8749a36cfec860d52
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-11-17 13:41:08 +0200

    Update comments.php

--- comments.php
+++ comments.php
@@ -8,17 +8,17 @@
 </h3>
 <section class="commentlist">
     <?php wp_list_comments( array(
-'style'             => 'div',
-'short_ping'        => true,
-'avatar_size'       => 40,
-'callback'          => 'bones_comments',
-'type'              => 'all',
-'reply_text'        => __('Reply', 'bonestheme'),
-'page'              => '',
-'per_page'          => '',
-'reverse_top_level' => null,
-'reverse_children'  => ''
-)); ?>
+    'style'             => 'div',
+    'short_ping'        => true,
+    'avatar_size'       => 40,
+    'callback'          => 'bones_comments',
+    'type'              => 'all',
+    'reply_text'        => __('Reply', 'bonestheme'),
+    'page'              => '',
+    'per_page'          => '',
+    'reverse_top_level' => null,
+    'reverse_children'  => ''
+    )); ?>
 </section>
 <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 <nav class="navigation comment-navigation" role="navigation">
@@ -32,7 +32,7 @@
 <?php endif; ?>
 <?php if ( ! comments_open() ) : ?>
 <p class="no-comments">
-    <?php _e( 'Comments are closed.' , 'bonestheme' ); ?>
+<?php _e( 'Comments are closed.' , 'bonestheme' ); ?>
 </p>
 <?php endif; ?>
 <?php endif; ?>
