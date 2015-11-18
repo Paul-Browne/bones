1cc824f0f98e9ea32a2ce9511bb5f81381e1fca9
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-11-18 13:21:54 +0200

    Update archive-custom_type.php

--- archive-custom_type.php
+++ archive-custom_type.php
@@ -1,3 +1,17 @@
+<?php
+/*
+ * CUSTOM POST TYPE ARCHIVE TEMPLATE
+ *
+ * This is the custom post type archive template. If you edit the custom post type name,
+ * you've got to change the name of this template to reflect that name change.
+ *
+ * For Example, if your custom post type is called "register_post_type( 'bookmarks')",
+ * then your template name should be archive-bookmarks.php
+ *
+ * For more info: http://codex.wordpress.org/Post_Type_Templates
+*/
+?>
+
 <?php get_header(); ?>
 <div id="content">
     <div id="inner-content">
