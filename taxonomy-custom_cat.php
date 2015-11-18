80f4fabf5eda328d2ba2d94e243f0c9f0f81a411
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-11-18 13:22:19 +0200

    Update taxonomy-custom_cat.php

--- taxonomy-custom_cat.php
+++ taxonomy-custom_cat.php
@@ -1,3 +1,17 @@
+<?php
+/*
+ * CUSTOM POST TYPE TAXONOMY TEMPLATE
+ *
+ * This is the custom post type taxonomy template. If you edit the custom taxonomy name,
+ * you've got to change the name of this template to reflect that name change.
+ *
+ * For Example, if your custom taxonomy is called "register_taxonomy('shoes')",
+ * then your template name should be taxonomy-shoes.php
+ *
+ * For more info: http://codex.wordpress.org/Post_Type_Templates#Displaying_Custom_Taxonomies
+*/
+?>
+
 <?php get_header(); ?>
 <div id="content">
     <div id="inner-content">
